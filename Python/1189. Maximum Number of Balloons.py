f3f25ff74b02cb671aee724a928ef18f951302e8
sotamatsuoca <sota.matsuoca@gmail.com>
2026-06-22 21:05:55 +0900

    Add solution for Maximum Number of Balloons

---
+++ Python/1189. Maximum Number of Balloons.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counts = Counter(text)
+        
+        return min(
+            counts['b'], 
+            counts['a'], 
+            counts['l'] // 2, 
+            counts['o'] // 2, 
+            counts['n']
+        )
