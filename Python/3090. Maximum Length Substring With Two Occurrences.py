dccd70985838e0697f3ddb2c98e1e05b2499326c
Sota Matsuoka <sota.matsuoca@gmail.com>
2026-08-14 23:18:09 +0900

    Add solution for Maximum Length Substring With Two Occurrences.py

---
+++ Python/3090. Maximum Length Substring With Two Occurrences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        count = {}
+        left = 0
+        result = 0
+        for right in range(len(s)):
+            count[s[right]] = count.get(s[right], 0) + 1
+            while count[s[right]] > 2:
+                count[s[left]] -= 1
+                left += 1
+            result = max(result, right - left + 1)
+        return result
